0c66b848c7aee6d6e3315835ba8ffa0aeedeb43d
Vincent Toon <vincenttoon22@gmail.com>
2024-01-12 16:48:30 -0600

    fully functioning starter framework

--- src/App.js
+++ src/App.js
@@ -51,6 +51,16 @@ function App() {
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, [correctLetters, wrongLetters, playable]);
 
+  function playAgain() {
+    setPlayable(true);
+
+    // empty arrays
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
   return (
     <div className="App">
       <Header />
@@ -59,7 +69,7 @@ function App() {
         <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
-      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} />
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain}/>
       <Notification showNotification={showNotification} />
     </div>
   );
