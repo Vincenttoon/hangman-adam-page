ffb5fc82ab9dad222414714cf224d3fc8b392f8e
Vincent Toon <vincenttoon22@gmail.com>
2024-01-12 15:45:18 -0600

    initial components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
 
 function App() {
-  return (
-    <div>
+  const words = ["application", "programming", "interface", "wizard"];
 
-    </divclassName=>
+  let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  let playable = true;
+
+  const correctLetters = [];
+  const wrongLetters = [];
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Notification.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Notification = () => {
+  return (
+    <div>
+      <div class="notification-container" id="notification-container">
+        <p>You have already entered this letter</p>
+      </div>
+    </div>
+  );
+};
+
+export default Notification;
